bda429d16d72f78b94c2a2d71eb088af47e28c58
igorhghd <igorhenrique.gomes@hotmail.com>
2026-08-22 19:46:09 -0300

    initial project commit

---
+++ src/main/java/henrique/igor/iiot/IndustrialIotBackendApplication.java
@@ -0,0 +1,13 @@
+package henrique.igor.iiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustrialIotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustrialIotBackendApplication.class, args);
+    }
+
+}
